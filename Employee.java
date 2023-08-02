ca9f42d514076dba9663c0acb091df8d6cbb8210
Letícia Paes <lepaeseng@gmail.com>
2023-08-02 14:12:40 -0300

    segunda versão

--- Employee.java
+++ Employee.java
@@ -1,16 +1,29 @@
 package crud;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Scanner;
+
 public class Employee {
 	
 	private int empno;
 	private String ename;
 	private int salary;
 	
-	Employee(int empno, String ename, int salary){
+	public Employee() {
+		
+	}
+	
+	public Employee(int empno, String ename, int salary){
 		this.empno = empno;
 		this.ename = ename;
 		this.salary = salary;
 	}
+	
+	Scanner sc = new Scanner(System.in);
+	List<Employee> c = new ArrayList<Employee>();
 	
 	public int getEmpno() {
 		return empno;
@@ -26,6 +39,101 @@ public class Employee {
 	
 	public String toString() {
 		return empno+ " " +ename+" "+salary;
+	}
+	
+	public void insert() {
+		
+		System.out.print("Enter Empno: ");
+		int eno = sc.nextInt();
+		sc.nextLine();
+		System.out.print("Enter EmpName: ");
+		String ename = sc.nextLine();
+		System.out.print("Enter Salary: ");
+		int salary = sc.nextInt();
+		c.add(new Employee(eno,ename,salary));
+		
+	}
+	
+	public void display() {
+		System.out.println("------------------------------");
+		Iterator<Employee> i = c.iterator();
+		while(i.hasNext()) {
+			Employee e = i.next();
+			System.out.println(e);
+		}
+		System.out.println("------------------------------");
+		
+	}
+	
+	public void search() {
+		boolean found = false;
+		System.out.println("Enter Empno to search: ");
+		int empno = sc.nextInt();
+		System.out.println("------------------------------");
+		Iterator<Employee> i = c.iterator();
+		while(i.hasNext()) {
+			Employee e = i.next();
+			if(e.getEmpno() == empno) {
+				System.out.println(e);
+				found = true;
+			}
+		}
+		if(!found) {
+			System.out.println("Record not found");
+		
+		}
+		System.out.println("------------------------------");
+		
+	}
+	
+	public void delete() {
+		Boolean found = false;
+		System.out.println("Enter Empno to delete: ");
+		empno = sc.nextInt();
+		System.out.println("------------------------------");
+		Iterator<Employee> i = c.iterator();
+		while(i.hasNext()) {
+			Employee e = i.next();
+			if(e.getEmpno() == empno) {
+				i.remove();
+				System.out.println("Record deleted!");
+				found = true;
+			} 
+		}
+		if(!found) {
+			System.out.println("Record not found");
+		
+		}
+		System.out.println("------------------------------");
+		
+	}
+	
+	public void update() {
+		Boolean found = false;
+		System.out.println("Enter Empno to update: ");
+		empno = sc.nextInt();
+		sc.nextLine();
+		System.out.println("------------------------------");
+		ListIterator<Employee> li = c.listIterator();
+		while(li.hasNext()) {
+			Employee e = li.next();
+			if(e.getEmpno() == empno) {
+				System.out.println("Enter new name: ");
+				ename = sc.nextLine();
+				System.out.println("Enter new salary: ");
+				salary = sc.nextInt();
+				li.set(new Employee(empno,ename,salary));
+				found = true;
+			
+				System.out.println("Record updated!");
+			}
+		}
+		if(!found) {
+			System.out.println("Record not found");
+		
+		}
+		System.out.println("------------------------------");
+		
 	}
 
 }
--- IndianDemo.java
+++ IndianDemo.java
@@ -1,17 +1,13 @@
 package crud;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
 import java.util.Scanner;
 
 public class IndianDemo {
 
+	
 	public static void main(String[] args) {
 		
-		Scanner s = new Scanner(System.in);
-		Scanner s1 = new Scanner(System.in);
-		List<Employee> c = new ArrayList<Employee>();
+		Employee emp = new Employee();
+		Scanner sc = new Scanner(System.in);
 		
 		int ch;
 		do {
@@ -21,99 +17,29 @@ public class IndianDemo {
 			System.out.println("4. DELETE");
 			System.out.println("5. UPDATE");
 			System.out.println("Enter your choice: ");
-			ch = s.nextInt();
+			ch = sc.nextInt();
 			
 			switch(ch) {
 				case 1:
-					System.out.print("Enter Empno: ");
-					int eno = s.nextInt();
-					System.out.print("Enter EmpName: ");
-					String ename = s1.nextLine();
-					System.out.print("Enter Salary: ");
-					int salary = s.nextInt();
-					c.add(new Employee(eno,ename,salary));
+					emp.insert();
 					break;
 				case 2:
-					System.out.println("------------------------------");
-					Iterator<Employee> i = c.iterator();
-					while(i.hasNext()) {
-						Employee e = i.next();
-						System.out.println(e);
-					}
-					System.out.println("------------------------------");
+					emp.display();
 					break;
 				case 3:
-					boolean found = false;
-					System.out.println("Enter Empno to search: ");
-					int empno = s.nextInt();
-					System.out.println("------------------------------");
-					i = c.iterator();
-					while(i.hasNext()) {
-						Employee e = i.next();
-						if(e.getEmpno() == empno) {
-							System.out.println(e);
-							found = true;
-						}
-					}
-					if(!found) {
-						System.out.println("Record not found");
-					
-					}
-					System.out.println("------------------------------");
+					emp.search();
 				break;
 				case 4:
-					found = false;
-					System.out.println("Enter Empno to delete: ");
-					empno = s.nextInt();
-					System.out.println("------------------------------");
-					i = c.iterator();
-					while(i.hasNext()) {
-						Employee e = i.next();
-						if(e.getEmpno() == empno) {
-							i.remove();
-							found = true;
-						} else {
-							System.out.println("Record deleted!");
-						}
-					}
-					if(!found) {
-						System.out.println("Record not found");
-					
-					}
-					System.out.println("------------------------------");
+					emp.delete();
 				break;
 				case 5:
-					found = false;
-					System.out.println("Enter Empno to update: ");
-					empno = s.nextInt();
-					System.out.println("------------------------------");
-					ListIterator<Employee> li = c.listIterator();
-					while(li.hasNext()) {
-						Employee e = li.next();
-						if(e.getEmpno() == empno) {
-							System.out.println("Enter new name: ");
-							ename = s1.nextLine();
-							System.out.println("Enter new salary: ");
-							salary = s.nextInt();
-							li.set(new Employee(empno,ename,salary));
-							found = true;
-						
-							System.out.println("Record updated!");
-						}
-					}
-					if(!found) {
-						System.out.println("Record not found");
-					
-					}
-					System.out.println("------------------------------");
+					emp.update();
 				break;
 			}
 		} while(ch!=0);
 		
-		
-		
-		s.close();
-
+		sc.close();
 	}
+	
 
 }
